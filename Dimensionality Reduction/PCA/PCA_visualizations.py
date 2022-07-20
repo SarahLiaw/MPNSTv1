eb1cdcaaaf74a581b3067f16d336691218d9aa12
SarahLiaw <sarah.liaw8@gmail.com>
2022-07-19 17:01:45 -0700

    Updated PCA_visualizations.py

--- Dimensionality Reduction/PCA/PCA_visualizations.py
+++ Dimensionality Reduction/PCA/PCA_visualizations.py
@@ -6,7 +6,7 @@ import seaborn as sns
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-data_path = '/home/sarahl/PycharmProjects/MPNST_v1/data_v1/MPNST_v1_concat.csv'
+data_path = '/home/sarahl/PycharmProjects/MPNST_v1/data_v1/MPNST_concat_no_outliers.csv'
 data = pd.read_csv(data_path)
 data = data.iloc[:, 1:-1]
 print(data.keys())
@@ -15,11 +15,6 @@ print(data.columns)
 print(data.isnull().sum())
 print(data.describe())
 print(data.corr())
-# f, ax = plt.subplots(figsize=(20, 20))
-# sns.set(font_scale=1.25)
-# sns.heatmap(data.corr(), annot=True, fmt='.1f')
-# plt.show(block=True)
-# plt.interactive(False)
 
 sns.countplot(x='Diagnosis', data=data)
 # plt.show(block=True)
@@ -39,67 +34,67 @@ print(y_train.shape)
 
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-x_train_std = sc.fit_transform(x_train)
+x_train_ps = sc.fit_transform(x_train)
 x_test_std = sc.fit_transform(x_test)
-print(np.mean(x_train_std))
-print(np.std(x_train_std))
 
-# #Finding the covariance matrix which is M^T*M
-# cov_matrix = np.matmul(x_train_std.T, x_train_std)
-# print('The dimension of the covariance matrix is: ', cov_matrix.shape)
-#
-# from scipy.linalg import eigh
-# ev = eigh(cov_matrix, eigvals_only=True)
-# print(ev)
-#
+#find covariance matrix
+from scipy.linalg import eigh
+cov = np.matmul(x_train_ps.T, x_train_ps)
+eg = eigh(cov, eigvals_only=True)
+
+print(np.mean(x_train_ps))
+print(np.std(x_train_ps))
+print(eg)
+
 
 from sklearn.decomposition import PCA
 pca = PCA()
-pca.fit_transform(x_train_std)
-percent_var = np.round(pca.explained_variance_ratio_*100, decimals=1)
-#calculating the percentage of variation that each principal component acco)
-labels = ['PC'+str(p) for p in range(1,len(percent_var)+1)]
+pca.fit_transform(x_train_ps)
 
+percent_var = np.round(pca.explained_variance_ratio_*100, decimals=1)
+# calc of variation % of each PC
+labels = ['PC'+str(p) for p in range(1,len(percent_var)+1)]
 f, ax = plt.subplots(figsize=(23, 5))
 plt.bar(x=range(1,len(percent_var)+1), height=percent_var, tick_label=labels)
 plt.xlabel('Principal Components')
-plt.ylabel('Percentage of Variation Explained')
-plt.title('Scree Plot showing all components')
+plt.ylabel('Variation %')
+plt.title('Scree Plot: All n PCs')
 plt.show()
 
-from sklearn.decomposition import PCA
+
 pca = PCA(n_components=3)
-pca.fit_transform(x_train_std)
+pca.fit_transform(x_train_ps)
 
 percent_var = np.round(pca.explained_variance_ratio_*100, decimals=1)
 labels = ['PC'+str(p) for p in range(1,len(percent_var)+1)]
 f, ax = plt.subplots(figsize=(8,5))
 plt.bar(x=range(1,len(percent_var)+1), height=percent_var, tick_label=labels)
+
 plt.xlabel('Principal Components')
-plt.ylabel('Percentage of Variation explained')
-plt.title('Scree Plot showing first 3 components')
+plt.ylabel('Variation %')
+plt.title('Scree Plot: n=3 PC')
 plt.show()
 
-explained_var = pca.explained_variance_ratio_
-print(explained_var)
+explained = pca.explained_variance_ratio_
+print(explained)
 
-cum_var = np.cumsum(np.round(explained_var, decimals = 3))
-cum_var_perc = cum_var*100
+cum = np.cumsum(np.round(explained, decimals = 3))
+cum_perc = cum*100
 pc_df = pd.DataFrame(['PC1','PC2', 'PC3'], columns=['PC'])
-explained_var_df = pd.DataFrame(explained_var, columns=['Explained variance'])
-cum_var_df = pd.DataFrame(cum_var_perc, columns=['Cumulative variance (in %)'])
-total_var_explained = pd.concat([pc_df, explained_var_df, cum_var_df], axis=1)
-print(total_var_explained)
-pca_data = pca.fit_transform(x_train_std)
+explained_df = pd.DataFrame(explained, columns=['Explained variance'])
+cum_df = pd.DataFrame(cum_perc, columns=['Cumulative variance (in %)'])
+total_explained = pd.concat([pc_df, explained_df, cum_df], axis=1)
+print(total_explained)
+pca_data = pca.fit_transform(x_train_ps)
 print('The reduced data is of the dimension: ', pca_data.shape)
 pca_1 = PCA(n_components=2)
-pca_data_1 = pca_1.fit_transform(x_train_std)
-pca_data_1 = np.vstack((pca_data_1.T, y_train)).T
+data_1 = pca_1.fit_transform(x_train_ps)
+data_1 = np.vstack((data_1.T, y_train)).T
 
 #creating a new dataframe for plotting the labeled points
-pca_df = pd.DataFrame(data=pca_data_1, columns=('1st Principal', '2nd Principal', 'Labels'))
+pca_df = pd.DataFrame(data=data_1, columns=('1st Principal', '2nd Principal', 'Labels'))
 print(pca_df.head())
 
 #visualizing the 2D points
-sns.FacetGrid(pca_df, hue='Labels', size=6).map(plt.scatter, '1st Principal', '2nd Principal').add_legend()
+sns.FacetGrid(pca_df, hue='Labels', height=6).map(plt.scatter, '1st Principal', '2nd Principal').add_legend()
 plt.show()
