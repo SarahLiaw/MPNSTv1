4ca41a7dfa96116b418210cda0d3c1ea1197ea4e
SarahLiaw <sarah.liaw8@gmail.com>
2022-08-09 21:45:39 -0700

    Trying to perform PCA but cumulative variance too high

--- pipeline/DELFI score/testing_data.py
+++ pipeline/DELFI score/testing_data.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+import numpy as np
+
+import matplotlib.pyplot as plt
 arm_df = pd.read_csv('/home/sarahl/PycharmProjects/MPNST_v1/data_v1/arm_frag.tsv', sep='\t')
 
 library_id_set = set(arm_df['library'])
@@ -33,3 +36,145 @@ print(check == mpnst_ids)
 # for models to converge with our limited cohort size.
 
 # if chr are the same in the library, find the average for each chrom
+
+
+# code for PCA over the delfi fragment ratios computed and select the minimum number of PCs to get 90% of the variance.
+
+data_path = '/home/sarahl/PycharmProjects/MPNST_v1/data_v1/delfi_ratio_w_diagnosis.csv'
+data = pd.read_csv(data_path)
+data = data.iloc[:, 1:-1]
+
+y = data.Diagnosis
+x = data.iloc[:, 1:]
+print(x)
+print(y)
+print(data.columns)
+print(data.keys())
+print(data.head())
+print(data.corr())
+
+
+
+from sklearn.model_selection import cross_validate
+
+
+def cross_validation(model, _X, _y, _cv=5):
+    '''Function to perform 5 Folds Cross-Validation
+     Parameters
+     ----------
+    model: Python Class, default=None
+            This is the machine learning algorithm to be used for training.
+    _X: array
+         This is the matrix of features.
+    _y: array
+         This is the target variable.
+    _cv: int, default=5
+        Determines the number of folds for cross-validation.
+     Returns
+     -------
+     The function returns a dictionary containing the metrics 'accuracy', 'precision',
+     'recall', 'f1' for both training set and validation set.
+    '''
+    _scoring = ['accuracy', 'precision', 'recall', 'f1']
+    results = cross_validate(estimator=model,
+                             X=_X,
+                             y=_y,
+                             cv=_cv,
+                             scoring=_scoring,
+                             return_train_score=True)
+
+    return {"Training Accuracy scores": results['train_accuracy'],
+            "Mean Training Accuracy": results['train_accuracy'].mean() * 100,
+            "Training Precision scores": results['train_precision'],
+            "Mean Training Precision": results['train_precision'].mean(),
+            "Training Recall scores": results['train_recall'],
+            "Mean Training Recall": results['train_recall'].mean(),
+            "Training F1 scores": results['train_f1'],
+            "Mean Training F1 Score": results['train_f1'].mean(),
+            "Validation Accuracy scores": results['test_accuracy'],
+            "Mean Validation Accuracy": results['test_accuracy'].mean() * 100,
+            "Validation Precision scores": results['test_precision'],
+            "Mean Validation Precision": results['test_precision'].mean(),
+            "Validation Recall scores": results['test_recall'],
+            "Mean Validation Recall": results['test_recall'].mean(),
+            "Validation F1 scores": results['test_f1'],
+            "Mean Validation F1 Score": results['test_f1'].mean()
+            }
+
+    def plot_result(x_label, y_label, plot_title, train_data, val_data):
+        '''Function to plot a grouped bar chart showing the training and validation
+          results of the ML model in each fold after applying K-fold cross-validation.
+         Parameters
+         ----------
+         x_label: str,
+            Name of the algorithm used for training e.g 'Decision Tree'
+
+         y_label: str,
+            Name of metric being visualized e.g 'Accuracy'
+         plot_title: str,
+            This is the title of the plot e.g 'Accuracy Plot'
+
+         train_result: list, array
+            This is the list containing either training precision, accuracy, or f1 score.
+
+         val_result: list, array
+            This is the list containing either validation precision, accuracy, or f1 score.
+         Returns
+         -------
+         The function returns a Grouped Barchart showing the training and validation result
+         in each fold.
+        '''
+
+        # Set size of plot
+        plt.figure(figsize=(12, 6))
+        labels = ["1st Fold", "2nd Fold", "3rd Fold", "4th Fold", "5th Fold"]
+        X_axis = np.arange(len(labels))
+        ax = plt.gca()
+        plt.ylim(0.40000, 1)
+        plt.bar(X_axis - 0.2, train_data, 0.4, color='blue', label='Training')
+        plt.bar(X_axis + 0.2, val_data, 0.4, color='red', label='Validation')
+        plt.title(plot_title, fontsize=30)
+        plt.xticks(X_axis, labels)
+        plt.xlabel(x_label, fontsize=14)
+        plt.ylabel(y_label, fontsize=14)
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+# https://machinelearningmastery.com/how-to-configure-k-fold-cross-validation/
+from sklearn.decomposition import PCA
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+label_encoder = LabelEncoder()
+encoded_y = label_encoder.fit_transform(y)
+label_encoder_name_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
+print("Mapping of Label Encoded Classes", label_encoder_name_mapping, sep="\n")
+print(encoded_y)
+
+sc = StandardScaler()
+x_scale = sc.fit_transform(x)
+pca = PCA()
+
+pca.fit_transform(x_scale)
+percent_var = np.round(pca.explained_variance_ratio_*100, decimals=1)
+# calc of variation % of each PC
+labels = ['PC'+str(p) for p in range(1,len(percent_var)+1)]
+f, ax = plt.subplots(figsize=(23, 5))
+plt.bar(x=range(1,len(percent_var)+1), height=percent_var, tick_label=labels)
+plt.xlabel('Principal Components')
+plt.ylabel('Variation %')
+plt.title('Scree Plot: All n PCs')
+plt.show()
+
+
+explained = pca.explained_variance_ratio_
+print(explained)
+
+cum = np.cumsum(np.round(explained, decimals = 3))
+cum_perc = cum*100
+pc_df = pd.DataFrame(['PC1','PC2', 'PC3'], columns=['PC'])
+explained_df = pd.DataFrame(explained, columns=['Explained variance'])
+cum_df = pd.DataFrame(cum_perc, columns=['Cumulative variance (in %)'])
+total_explained = pd.concat([pc_df, explained_df, cum_df], axis=1)
+print(total_explained)
