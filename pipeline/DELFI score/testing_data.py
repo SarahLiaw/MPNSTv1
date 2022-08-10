f7a8834e40f7ba216c1f703827d76cb68b4ffcb6
SarahLiaw <sarah.liaw8@gmail.com>
2022-08-10 11:32:27 -0700

    Changes

--- pipeline/DELFI score/testing_data.py
+++ pipeline/DELFI score/testing_data.py
@@ -2,8 +2,11 @@ import pandas as pd
 
 import numpy as np
 
+from sklearn.model_selection import cross_validate, StratifiedKFold, KFold
+
+
 import matplotlib.pyplot as plt
-arm_df = pd.read_csv('/home/sarahl/PycharmProjects/MPNST_v1/data_v1/arm_frag.tsv', sep='\t')
+arm_df = pd.read_csv('/home/sarahl/PycharmProjects/MPNST_v1/data_v1/delfi_ratio.csv')
 
 library_id_set = set(arm_df['library'])
 #print(library_id_set)
@@ -19,9 +22,9 @@ for id in library_id_set:
 #print(arm_df)
 check = sorted(set(arm_df['library']))
 
-#print(check)
-#print(mpnst_ids)
-#print(check == mpnst_ids)
+print(check)
+print(mpnst_ids)
+print(check == mpnst_ids)
 
 # basically removed all the rows of the data for the library-ids that are not relevant in the above.
 
@@ -50,20 +53,18 @@ check = sorted(set(arm_df['library']))
 
 data_path = '/home/sarahl/PycharmProjects/MPNST_v1/data_v1/delfi_ratio_w_diagnosis.csv'
 data = pd.read_csv(data_path)
-data = data.iloc[:, 1:-1]
-
+arm_df = arm_df.iloc[:, 1:-1]
 y = data.Diagnosis
-x = data.iloc[:, 1:]
+x = arm_df.iloc[:, 0:]
+print(x.shape)
+print(arm_df.shape)
+print(x.keys())
 #print(x)
 #print(y)
 # print(data.columns)
-# print(data.keys())
+print(arm_df.keys())
 # print(data.head())
 # print(data.corr())
-
-
-
-from sklearn.model_selection import cross_validate, StratifiedKFold, KFold
 
 
 def cross_validation(model, _X, _y, _cv=5):
@@ -161,35 +162,47 @@ print("Mapping of Label Encoded Classes", label_encoder_name_mapping, sep="\n")
 
 sc = StandardScaler()
 x_scale = sc.fit_transform(x)
-pca = PCA()
-
+print(x_scale)
+print(x_scale.shape)
 #kf =KFold(n_splits=5, shuffle=True, random_state=1)
 kf = KFold(n_splits=5)
-counter =1
-for train_index, test_index in kf.split(x, y):
-     print("TRAIN:", train_index, "TEST:", test_index)
-     X_train, X_test = x_scale[train_index], x_scale[test_index]
-     y_train, y_test = encoded_y[train_index], encoded_y[test_index]
-     pca = PCA()
-     pca_X = pca.fit_transform(X_train)
 
-     explained = pca.explained_variance_ratio_
-     print(explained)
+# for train_index, test_index in kf.split(x_scale, y):
+#      print("TRAIN:", train_index, "TEST:", test_index)
+#      X_train, X_test = x_scale[train_index], x_scale[test_index]
+#      y_train, y_test = encoded_y[train_index], encoded_y[test_index]
+#
+#      pca = PCA()
+#      pca.fit_transform(X_train)
+#
+#      explained = pca.explained_variance_ratio_
+#      print(explained)
+#
+#      cum = np.cumsum(np.round(explained, decimals=3))
+#      cum_perc = cum * 100
+#      pc_df = pd.DataFrame(['PC1', 'PC2', 'PC3'], columns=['PC'])
+#      explained_df = pd.DataFrame(explained, columns=['Explained variance'])
+#      cum_df = pd.DataFrame(cum_perc, columns=['Cumulative variance (in %)'])
+#      total_explained = pd.concat([pc_df, explained_df, cum_df], axis=1)
+#      print(total_explained)
 
-     cum = np.cumsum(np.round(explained, decimals=3))
-     cum_perc = cum * 100
-     pc_df = pd.DataFrame(['PC1', 'PC2', 'PC3'], columns=['PC'])
-     explained_df = pd.DataFrame(explained, columns=['Explained variance'])
-     cum_df = pd.DataFrame(cum_perc, columns=['Cumulative variance (in %)'])
-     total_explained = pd.concat([pc_df, explained_df, cum_df], axis=1)
-     print(total_explained)
 
-     counter+=1
+pca = PCA()
+pca.fit(x)
+explained = pca.explained_variance_ratio_
+print(explained)
 
-
-pca.fit_transform(x_scale)
+cum = np.cumsum(np.round(explained, decimals=3))
+cum_perc = cum * 100
+pc_df = pd.DataFrame(['PC1', 'PC2', 'PC3'], columns=['PC'])
+explained_df = pd.DataFrame(explained, columns=['Explained variance'])
+cum_df = pd.DataFrame(cum_perc, columns=['Cumulative variance (in %)'])
+total_explained = pd.concat([pc_df, explained_df, cum_df], axis=1)
+print(total_explained)
 percent_var = np.round(pca.explained_variance_ratio_*100, decimals=1)
 # calc of variation % of each PC
+
+
 labels = ['PC'+str(p) for p in range(1,len(percent_var)+1)]
 f, ax = plt.subplots(figsize=(23, 5))
 plt.bar(x=range(1,len(percent_var)+1), height=percent_var, tick_label=labels)
@@ -197,15 +210,3 @@ plt.xlabel('Principal Components')
 plt.ylabel('Variation %')
 plt.title('Scree Plot: All n PCs')
 plt.show()
-
-
-explained = pca.explained_variance_ratio_
-print(explained)
-
-cum = np.cumsum(np.round(explained, decimals = 3))
-cum_perc = cum*100
-pc_df = pd.DataFrame(['PC1','PC2', 'PC3'], columns=['PC'])
-explained_df = pd.DataFrame(explained, columns=['Explained variance'])
-cum_df = pd.DataFrame(cum_perc, columns=['Cumulative variance (in %)'])
-total_explained = pd.concat([pc_df, explained_df, cum_df], axis=1)
-print(total_explained)
