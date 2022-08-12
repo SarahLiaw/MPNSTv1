3d69432692edcad63b64f39e59f0de061e505661
SarahLiaw <sarah.liaw8@gmail.com>
2022-08-12 13:42:32 -0700

    Push

--- pipeline/DELFI score/testing_data.py
+++ pipeline/DELFI score/testing_data.py
@@ -53,16 +53,16 @@ print(check == mpnst_ids)
 
 data_path = '/home/sarahl/PycharmProjects/MPNST_v1/data_v1/delfi_ratio_w_diagnosis.csv'
 data = pd.read_csv(data_path)
-arm_df = arm_df.iloc[:, 1:-1]
+data = data.iloc[:, 1:-1]
 y = data.Diagnosis
-x = arm_df.iloc[:, 0:]
-print(x.shape)
-print(arm_df.shape)
-print(x.keys())
-#print(x)
-#print(y)
-# print(data.columns)
-print(arm_df.keys())
+x = data.iloc[:, 1:]
+# print(x.shape)
+# print(.shape)
+# print(x.keys())
+# #print(x)
+# #print(y)
+# # print(data.columns)
+# print(arm_df.keys())
 # print(data.head())
 # print(data.corr())
 
@@ -167,24 +167,24 @@ print(x_scale.shape)
 #kf =KFold(n_splits=5, shuffle=True, random_state=1)
 kf = KFold(n_splits=5)
 
-# for train_index, test_index in kf.split(x_scale, y):
-#      print("TRAIN:", train_index, "TEST:", test_index)
-#      X_train, X_test = x_scale[train_index], x_scale[test_index]
-#      y_train, y_test = encoded_y[train_index], encoded_y[test_index]
-#
-#      pca = PCA()
-#      pca.fit_transform(X_train)
-#
-#      explained = pca.explained_variance_ratio_
-#      print(explained)
-#
-#      cum = np.cumsum(np.round(explained, decimals=3))
-#      cum_perc = cum * 100
-#      pc_df = pd.DataFrame(['PC1', 'PC2', 'PC3'], columns=['PC'])
-#      explained_df = pd.DataFrame(explained, columns=['Explained variance'])
-#      cum_df = pd.DataFrame(cum_perc, columns=['Cumulative variance (in %)'])
-#      total_explained = pd.concat([pc_df, explained_df, cum_df], axis=1)
-#      print(total_explained)
+for train_index, test_index in kf.split(x_scale, y):
+     print("TRAIN:", train_index, "TEST:", test_index)
+     X_train, X_test = x_scale[train_index], x_scale[test_index]
+     y_train, y_test = encoded_y[train_index], encoded_y[test_index]
+
+     pca = PCA()
+     pca.fit_transform(X_train)
+
+     explained = pca.explained_variance_ratio_
+     print(explained)
+
+     cum = np.cumsum(np.round(explained, decimals=3))
+     cum_perc = cum * 100
+     pc_df = pd.DataFrame(['PC1', 'PC2', 'PC3'], columns=['PC'])
+     explained_df = pd.DataFrame(explained, columns=['Explained variance'])
+     cum_df = pd.DataFrame(cum_perc, columns=['Cumulative variance (in %)'])
+     total_explained = pd.concat([pc_df, explained_df, cum_df], axis=1)
+     print(total_explained)
 
 
 pca = PCA()
@@ -210,3 +210,8 @@ plt.xlabel('Principal Components')
 plt.ylabel('Variation %')
 plt.title('Scree Plot: All n PCs')
 plt.show()
+
+
+# NOTE THAT INSTEAD of doing standard scalar over all features, do it over each sample since there are multiple chromosomes
+
+# refer to the mathios paper (OR SLACK) for more information.
